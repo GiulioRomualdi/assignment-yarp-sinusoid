bed92b96f8768ca3f636002e266cd0833bca2e4c
GiulioRomualdi <giulio.romualdi@gmail.com>
2017-12-22 15:17:51 +0100

    add singnal properties into config.ini file

--- producer_module/producer.cpp
+++ producer_module/producer.cpp
@@ -1,84 +1,88 @@
+/* -*- mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
 #include <yarp/os/Network.h>
 #include <yarp/os/RFModule.h>
 #include <yarp/os/Time.h>
 
 #include <math.h>
 
-// frequency and amplitude of the sinusoidal signal
-#define FREQUENCY 1
-#define AMPLITUDE 1
-
 using namespace yarp::os;
 class ProducerModule:public RFModule
 {
-  yarp::os::Port SignalPort;
+    yarp::os::Port SignalPort;
 
-  double t0;
+    double frequency;
+    double amplitude;
+    double t0;
   
 public:
-  double getPeriod()
-  {
-    //  period of the module (seconds)
-    return 0.01;
-  }
+    double getPeriod()
+    {
+        //  period of the module (seconds)
+        return 0.01;
+    }
 
-  bool updateModule()
-  {
-    // get the current time
-    double t = yarp::os::Time::now();
+    bool updateModule()
+    {
+        // get the current time
+        double t = yarp::os::Time::now();
 
-    // evaluate the signal
-    double signal = AMPLITUDE * sin(2 * M_PI * FREQUENCY * (t - t0));
+        // evaluate the signal
+        double signal = amplitude * sin(2 * M_PI * frequency * (t - t0));
 
-    // prepare the message
-    Bottle msg;
-    msg.addDouble((t - t0));
-    msg.addDouble(signal);
+        // prepare the message
+        Bottle msg;
+        msg.addDouble((t - t0));
+        msg.addDouble(signal);
 
-    // send the message
-    SignalPort.write(msg);
-  }
+        // send the message
+        SignalPort.write(msg);
+    }
 
-  bool configure(yarp::os::ResourceFinder &rf)
-  {
-    // open the Signal port
-    SignalPort.open("/producer/signal:o");
+    bool configure(yarp::os::ResourceFinder &rf)
+    {
+        // open the Signal port
+        SignalPort.open("/producer/signal:o");
 
-    // get init time
-    t0 = yarp::os::Time::now();
+        amplitude = rf.find("amplitude").asDouble();
+        frequency = rf.find("frequency").asDouble();
 
-    return true;
-  }
+        // get init time
+        t0 = yarp::os::Time::now();
+
+        return true;
+    }
   
   
-  bool interruptModule()
-  {
-    return true;
-  }
+    bool interruptModule()
+    {
+        return true;
+    }
   
-  bool close()
-  {
-    // close port explicitly
-    SignalPort.close();
+    bool close()
+    {
+        // close port explicitly
+        SignalPort.close();
 
-    return true;
-  }
+        return true;
+    }
 };
 
 int main(int argc, char * argv[])
 {
-  // initialize yarp network
-  Network yarp;
+    // initialize yarp network
+    Network yarp;
   
-  // create the producer module 
-  ProducerModule module;
+    // create the producer module 
+    ProducerModule module;
   
-  // prepare and configure the resource finder
-  ResourceFinder rf;
-  rf.configure(argc, argv);
-  rf.setVerbose(true);
+    // prepare and configure the resource finder
+    ResourceFinder rf;
+    rf.setVerbose(true);
+    rf.setDefaultConfigFile("config.ini");
+    rf.configure(argc, argv);
 
-  module.runModule(rf);   
+    module.runModule(rf);   
 
-  return 0;
+    return 0;
 }
